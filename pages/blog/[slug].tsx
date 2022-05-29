7a5e12be51965e643ee1f7099ee7eb6300b2ec86
Debby Ji <jasmine820921@gmail.com>
2022-05-29 15:56:18 +0800

    feat: add example page

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,55 @@
+import markdownToHtml from "../../utils/markdownToHtml"
+import { getAllPosts, getPostBySlug } from "../api/post"
+
+
+
+export default function BlogPost({post}) {
+
+return (
+
+    <div>
+    <div
+   
+    dangerouslySetInnerHTML={{ __html: post.content }}
+  /></div>
+)
+}
+
+
+export async function getStaticProps({ params }) {
+    const post = getPostBySlug(params.slug, [
+      'title',
+      'date',
+      'slug',
+      'author',
+      'content',
+      'ogImage',
+      'coverImage',
+    ])
+    const content = await markdownToHtml(post.content || '')
+  
+    return {
+      props: {
+        post: {
+          ...post,
+          content,
+        },
+      },
+    }
+  }
+
+  export async function getStaticPaths() {
+    const posts = getAllPosts(['slug'])
+  
+    return {
+      paths: posts.map((post) => {
+        return {
+          params: {
+            slug: post.slug,
+          },
+        }
+      }),
+      fallback: false,
+    }
+  }
+
